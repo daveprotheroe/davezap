2f95d6c72bb94cb50479aae91ff0c8c98de37fe0
daveprotheroe <dave.protheroe@bbc.co.uk>
2018-10-11 09:28:49 +0100

    Update Thursday

--- davezap2.js
+++ davezap2.js
@@ -3,7 +3,10 @@ let leftkeydown = false;
 let rightkeydown = false;
 let spacekeydown = false;
 let framecount = 0;
-let bugSpeed = 120;
+let bugDelay = 120;
+let score = 0;
+let gameState = 0;
+let spacewaslifted = false;
 
 // This is the function canon.
 const canvas = document.getElementById ("canvas1"); 
@@ -25,6 +28,32 @@ let bug = {
 
 var ctx = canvas.getContext("2d");
 document.addEventListener("keydown", keydownhandler);
+
+function init(){
+    leftkeydown = false;
+    rightkeydown = false;
+    spacekeydown = false;
+    framecount = 0;
+    bugDelay = 120;
+    score = 0;
+    spacewaslifted = false;
+
+        canon = { 
+            x : 0.5 * canvas.width,
+            width : 0.05 * canvas.width,
+            height : 0.025 * canvas.height,
+            y : 0.9 * canvas.height
+    }; 
+
+    bug = { 
+        x : 0.5 * canvas.width,
+        width : 0.05 * canvas.width,
+        height : 0.05 * canvas.height,
+        y : 0.1 * canvas.height,
+        alive : true
+        }; 
+
+    }
 
 // a handler to rember the state of the cursor keys
 function keydownhandler (event) {
@@ -59,6 +88,10 @@ function cleanScreen() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function drawScore() {
+    ctx.fillText (score * 10,0,10);
+}
+
 function drawCanon() {
     ctx.fillRect (canon.x - (canon.width * 0.5), canon.y, canon.width, canon.height);
     ctx.fillRect (canon.x - (canon.width * 0.5 * 0.25), canon.y - canon.height, canon.width * 0.25, canon.height);
@@ -84,23 +117,32 @@ function isBugHit() {
 }
 
 function drawBug() {
-    if ((framecount % bugSpeed) == 0){
+    if ((framecount % bugDelay) == 0){
         bug.x = Math.random() * (canvas.width - bug.width);
     }
     ctx.fillRect (bug.x, bug.y, bug.width, bug.height);
 }
 
-function gameLoop(timestamp) {
+function welcome() {
+    if (spacekeydown == true) {
+        gameState = 1;
+        init();
+    }
+
+}
+
+function playing() {
     framecount +=1;
     if(leftkeydown == true && canon.x >(canon.width * 0.5)) {
         canon.x -=8;
     }
+    //console.log(timestamp);
     if(rightkeydown == true && canon.x < (canvas.width - canon.width * 0.5)) {
         canon.x += 8;
     }
-
-
-
+    if (bug.y >= canon.y) {
+        gameState = 2;
+    }
     cleanScreen();
 
     if (bug.alive) {
@@ -110,14 +152,42 @@ function gameLoop(timestamp) {
     if (spacekeydown == true && (framecount % 3) == 0){
         drawLaser(); 
         if (isBugHit()) {
-            bug.alive = false;
+            score = (score + ((bug.y / canvas.height) * 100));
+            bug.y = 0.1 * canvas.height;
+            bug.x = Math.random() * (canvas.width - bug.width);
             console.log("zapped");
+            bugDelay = (bugDelay- 5);
         }   else {
-            bug.y +=(0.1 * canvas.height);
+            bug.y +=(0.03 * canvas.height);
         } 
     }
 
-      
+    drawScore();
+
+}
+
+function dead() {
+    cleanScreen();
+    if (spacekeydown == false) {
+        spacewaslifted = true
+    }
+    if (spacekeydown == true && spacewaslifted==true) {
+        gameState = 1;
+        init();
+        spacewaslifted = false;
+    }
+}
+
+function gameLoop(timestamp) {
+    if (gameState == 0) {
+        welcome();
+    }
+    if (gameState == 1) {
+        playing();
+    }
+    if (gameState == 2) {
+        dead();
+    }
     window.requestAnimationFrame(gameLoop); 
 }
 
